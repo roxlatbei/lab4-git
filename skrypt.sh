affef9c6d78aea871e57f2819710de43b3892c68
Roman Kosh <romankoszowskii@gmail.com>
2026-07-06 20:31:43 +0200

    Dodano flage --date wyswietlajaca aktualna date

--- skrypt.sh
+++ skrypt.sh
@@ -7,12 +7,19 @@
 SCRIPT_NAME=$(basename "$0")
 TODAY=$(date +"%Y-%m-%d")
 
+show_date() {
+    echo "Dzisiejsza data: $TODAY"
+}
+
 if [ $# -eq 0 ]; then
     echo "Brak argumentow. Uzyj --help aby zobaczyc dostepne opcje."
     exit 0
 fi
 
 case "$1" in
+    --date)
+        show_date
+        ;;
     *)
         echo "Nieznana opcja: $1"
         exit 1
